b8a040950cbdb6b8add3eeaa399f1d911b3d0a25
chaofiber <39195147+chaofiber@users.noreply.github.com>
2020-04-12 18:06:27 +0200

    finish half

--- project/compute_controller_base_parameters.m
+++ project/compute_controller_base_parameters.m
@@ -11,6 +11,7 @@ function param = compute_controller_base_parameters
     a1o = truck.a1o;
     a23 = truck.a23;
     a12 = truck.a12;
+    To = truck.To;
     Ac = [(-a12-a1o)/m1,            a12/m1,             0;
                  a12/m2, (-a12-a23-a2o)/m2,        a23/m2;
                       0,            a23/m3, (-a23-a3o)/m3];
@@ -18,8 +19,7 @@ function param = compute_controller_base_parameters
              0, 1/m2;
              0,    0];
     Bcd = diag([1/m1, 1/m2, 1/m3]);
-    dc = truck.w;
-    
+    dc = truck.w + [a1o*To; a2o*To;a3o*To];
     % (2) discretization
     Ts = 60;
     %A = eye(3) + Ts*Ac;
@@ -34,19 +34,19 @@ function param = compute_controller_base_parameters
     % T(2)=A(2,1)*T(1)+A(2,2)*T(2)+A(2,3)*T(3)+B(2,1)*P(1)+B(2,2)*p(2)+Bd(2,:)*dc;
     % T(3)=A(3,1)*T(1)+A(3,2)*T(2)+A(3,3)*T(3)+B(3,1)*P(1)+B(3,2)*p(2)+Bd(3,:)*dc;
     % Fx=e to solve for x=[T(3),p(1),p(2)]';
-    F = [  A(1,3),   B(1,1), B(1,2);
-           A(2,3),   B(2,1), B(2,2);
-         A(3,3)-1, B(3,1), B(3,2)];
-    e = [-21-A(1,1)*(-21)-A(1,2)*0.3-Bd(1,:)*dc;
-         0.3-A(2,1)*(-21)-A(2,2)*0.3-Bd(2,:)*dc;
-         -A(3,1)*(-21)-A(3,2)*0.3-Bd(3,:)*dc];
-    sol = linsolve(F,e);
+    %F = [  A(1,3),   B(1,1), B(1,2);
+    %       A(2,3),   B(2,1), B(2,2);
+    %     A(3,3)-1, B(3,1), B(3,2)];
+    %e = [-21-A(1,1)*(-21)-A(1,2)*0.3-Bd(1,:)*dc;
+    %    0.3-A(2,1)*(-21)-A(2,2)*0.3-Bd(2,:)*dc;
+    %     -A(3,1)*(-21)-A(3,2)*0.3-Bd(3,:)*dc];
+    %sol = linsolve(F,e);
     
-    T_sp = [-21; 0.3; sol(1)];
-    p_sp = [sol(2); sol(3)];
-    %T_sp = [-21; 0.3; -(Ac(3,2)*0.3+Bcd(3,3)*dc(3))/Ac(3,3)];
-    %p_sp = [                -(Ac(1,1)*(-21)+Ac(1,2)*0.3+Bcd(1,1)*dc(1));
-    %        -(Ac(2,1)*(-21)+Ac(2,2)*0.3+Ac(2,3)*T_sp(3)+Bcd(2,2)*dc(2))];
+    %T_sp = [-21; 0.3; sol(1)];
+    %p_sp = [sol(2); sol(3)];
+    T_sp = [-21; 0.3; -(Ac(3,2)*0.3+Bcd(3,3)*dc(3))/Ac(3,3)];
+    p_sp = [                -(Ac(1,1)*(-21)+Ac(1,2)*0.3+Bcd(1,1)*dc(1))*m1;
+            -(Ac(2,1)*(-21)+Ac(2,2)*0.3+Ac(2,3)*T_sp(3)+Bcd(2,2)*dc(2))*m2];
     
     % (4) system constraints
     Pcons = truck.InputConstraints;
@@ -57,11 +57,11 @@ function param = compute_controller_base_parameters
     Xcons = Tcons - T_sp;
     
     % (5) LQR cost function
-    Q = [1000,0,0;
-         0,10,0;
+    Q = [500,0,0;
+         0,100,0;
          0, 0, 0];
-    R = [0.001,0
-         0, 0.0001];
+    R = [0.01,0
+         0, 0.001];
     
     % put everything together
     param.A = A;
@@ -74,5 +74,10 @@ function param = compute_controller_base_parameters
     param.Xcons = Xcons;
     param.Tcons = Tcons;
     param.Pcons = Pcons;
+    
+    % compute and add other properties
+    [P,K,~] = idare(A, B, Q, R,[],[]);
+    param.P = P;
+    param.K = K;  % the controller should be u = -K * x
 end
 
--- project/run_simulations.m
+++ project/run_simulations.m
@@ -10,4 +10,6 @@ clear controller_lqr;
 % execute simulation starting from T0_1 using lqr controller with scenario 1
 param = compute_controller_base_parameters;
 T0_1 = param.T_sp+[3;1;0];
-[T, p] = simulate_truck(T0_1, @controller_lqr, scen1);
+T0_2 = param.T_sp+[-1;-0.3;-4.5];
+%[T, p] = simulate_truck(T0_2, @controller_lqr, scen1);
+[T, p] = simulate_truck(T0_2, @controller_mpc_1, scen1);
